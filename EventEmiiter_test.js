1151ef8181644d98dec6553cb7e87a1d0e36fd19
Clown Monster <mk6386223@gmail.com>
2020-04-01 23:53:03 +0530

    Update EventEmiiter_test.js

--- EventEmiiter_test.js
+++ EventEmiiter_test.js
@@ -1,6 +1,6 @@
-const EventEmitter = require('events');
+const EventEmitter = require('events'); // this is in ES5 . ES6 methods can also be used
 
-const EE = new EventEmitter();
+const EE = new EventEmitter(); // EE is an instantaneous object of class EventEmitter
 
 EE.on('test1',(n1,n2) =>{
     console.log(n1+n2);
